6aabfc1a9062060591b22dd151736632b1374b02
Bohdan <panchyshyn33@gmail.com>
2024-04-05 11:13:09 +0300

    add some function

--- src/student/controller.js
+++ src/student/controller.js
@@ -11,10 +11,30 @@ const getStudents = (req, res) => {
 
 const getStudentById = (req, res) => {
    const id = parseInt(req.params.id);
-   pool.query(queries.getStudentById, [id], (error, results) => {
-      if (error) throw error;
-      res.status(200).json(results.rows);
-   })
+   pool.query(queries.existsById, [id], (error, result)=> {
+      if(result.rows[0].count > 0) {
+         console.log("Executing search");
+         getStudentByIdMethod(res, id);
+         return;
+      }
+      console.log("Sending 400");
+      res.status(400).send();
+   });
+  
+}
+
+
+const getStudentByIdMethod = (res, ...arr) => {
+   pool.query(queries.getStudentById, arr, (error, results) => {
+   // if (error) throw error;
+   console.log(results);
+   const noFoundId = !results.rows.length;
+      if (noFoundId) {
+         res.status(400);
+         res.send("Not found");
+      }
+   res.status(200).json(results.rows).send();
+})
 }
 
 const addStudent = (req, res) => {
@@ -28,7 +48,10 @@ const addStudent = (req, res) => {
 
       //add student db
       pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
-         if (error) throw error;
+         if (error) {
+            res.status(400);
+            return;
+         };
          res.status(201).send('Student Created Successfully!');
       })
    });
@@ -51,10 +74,30 @@ const removeStudent = (req, res) => {
    })
 }
 
+
+const updateStudent = (req, res) => {
+   const id = parseInt(req.params.id);
+   const { name } = req.body;
+
+   pool.query(queries.getStudentById, [id], (error, results) => {
+      const noStudentFound = !results.rows.length;
+      if (noStudentFound) {
+         res.send('Student does not exists in the database');
+      }
+
+      pool.query(queries.updateStudent, [name, id], (error, results) => {
+         if (error) throw error;
+         res.status(200).send('Student updated successfully');
+      })
+   });
+   
+}
+
 module.exports = {
    getStudents,
    getStudentById,
    addStudent,
    removeStudent,
+   updateStudent,
 };
 
